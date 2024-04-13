198664604a2b3a0ee348464702bba19da6769448
Heewon Park <80496838+HeewonP825@users.noreply.github.com>
2024-04-14 01:43:51 +0900

    Title: 수의 곱셈, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

--- 240414/수의 곱셈/multiplication-of-numbers.kt
+++ 240414/수의 곱셈/multiplication-of-numbers.kt
@@ -1,24 +1,38 @@
 fun main() {
     val input = readLine() ?: ""
-    val nums = input.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }.toIntArray()
+    val num = input.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }.toIntArray()
 
-    if (nums.size < 3) {
+    if (num.size < 3) {
         return
     }
 
-    val products = mutableSetOf<Int>()
+    val array = IntArray(7)
 
-    for (i in nums.indices) {
-        for (j in i + 1 until nums.size) {
-            for (k in j + 1 until nums.size) {
-                products.add(nums[i] * nums[j] * nums[k])
-            }
-        }
+    for(i in 0 until 3)
+        array[i] = num[i]
+    
+    array[3] = num[0] * num[1];
+    array[4] = num[1] * num[2];
+    array[5] = num[0] * num[2];
+    array[6] = num[0] * num[1] * num[2];
+
+    val judge = Array(7) { IntArray(2) }
+    for(i in 0 until 7) {
+        judge[i][0] = array[i]
+        judge[i][1] = if(array[i] % 2 == 0) 0 else 1
     }
 
-    val sortedProducts = products.sortedWith(compareByDescending<Int> {
-        if (it % 2 == 0) 0 else 1
-    }.thenByDescending { it })
+    var oddRank = 0
+    var evenRank = 0
+    for(i in 0 until 7) {
+        if(judge[i][1] == 1 && judge[i][0] > oddRank)
+            oddRank = judge[i][0]
+        else if(judge[i][1] == 0 && judge[i][0] > evenRank)
+            evenRank = judge[i][0]
+    }
 
-    println(sortedProducts.first())
+    if(oddRank != 0)
+        println(oddRank)
+    else
+        println(evenRank)
 }
