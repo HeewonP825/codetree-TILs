8cf041085cf0b196432396f7c1580a4190e50f5a
Heewon Park <80496838+HeewonP825@users.noreply.github.com>
2024-04-15 23:28:57 +0900

    Title: 특정 조건의 수 뽑기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240415/특정 조건의 수 뽑기/draw-number-of-specific-conditions.c
+++ 240415/특정 조건의 수 뽑기/draw-number-of-specific-conditions.c
@@ -5,8 +5,8 @@ int main() {
     int n;
     scanf("%d", &n);
     
-    int closest = 200;  // 200보다 큰 수 중에서 가장 작은 수로 초기화
-    int above = -1;     // 100 이상의 수 중에서 가장 작은 수로 초기화
+    int closest = 200;  
+    int above = -1;    
 
     for (int i = 0; i < n; i++) {
         int num;
@@ -14,12 +14,14 @@ int main() {
         
         if (abs(100 - num) < abs(100 - closest)) {
             closest = num;
-        } else if (num >= 100 && (above == -1 || num < above)) {
+        }
+        
+        if (num >= 100 && (above == -1 || num < above)) {
             above = num;
         }
     }
 
-    printf("%d %d\n", closest, above == -1 ? -1 : above);
+    printf("%d %d\n", closest, above);
     
     return 0;
 }
