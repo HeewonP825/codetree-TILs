0fa5de989fc11141ef5c72d87cd70c27d7b9ba4e
Heewon Park <80496838+HeewonP825@users.noreply.github.com>
2024-04-22 23:03:25 +0900

    Title: 약수 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240422/약수 출력하기/print-divisor.c
+++ 240422/약수 출력하기/print-divisor.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++) {
+    int sqrt_n = sqrt(n);
+
+    for (int i = 1; i <= sqrt_n; i++) {
         if (n % i == 0) {
             printf("%d ", i);
+        }
+    }
+
+    for (int i = sqrt_n; i >= 1; i--) {
+        if (n % i == 0 && i != n / i) { 
+            printf("%d ", n / i);
         }
     }
 
