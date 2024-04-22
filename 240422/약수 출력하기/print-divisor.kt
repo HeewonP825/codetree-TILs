9133b91e134d5883c274e5e9a82312e0a399a194
Heewon Park <80496838+HeewonP825@users.noreply.github.com>
2024-04-22 23:04:09 +0900

    Title: 약수 출력하기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240422/약수 출력하기/print-divisor.kt
+++ 240422/약수 출력하기/print-divisor.kt
@@ -1,9 +1,19 @@
+import kotlin.math.sqrt
+
 fun main() {
     val n = readLine()!!.toInt()
 
-    for (i in 1..n) {
+    val sqrtN = sqrt(n.toDouble()).toInt()
+
+    for (i in 1..sqrtN) {
         if (n % i == 0) {
             print("$i ")
+        }
+    }
+
+    for (i in sqrtN downTo 1) {
+        if (n % i == 0 && i != n / i) { 
+            print("${n / i} ")
         }
     }
 }
